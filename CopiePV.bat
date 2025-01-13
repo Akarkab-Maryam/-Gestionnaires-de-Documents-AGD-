a4b99063f925773dce27f626806a72c897eef723
Akarkab-Maryam <ourzazat.ne1998@gmail.com>
2025-01-13 12:23:22 +0100

    Add files via upload

---
+++ CopiePV.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\maryam\Contacts\Desktop\AutomatREC"
+node CopiePV.js
+pause
---
+++ lancerScript.bat
@@ -0,0 +1,6 @@
+@echo off
+rem Le chemin du dossier où se trouve votre script
+cd /d "C:\Users\maryam\Contacts\Desktop\AutomatREC"
+rem Exécution du script Node.js
+node "C:\Users\maryam\Contacts\Desktop\AutomatREC\Recole.js"
+pause
